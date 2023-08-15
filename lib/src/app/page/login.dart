7616510c10d169cee214b9fb446b45e31aaed8f1
blackcowman <soh990301@gmail.com>
2023-08-15 16:53:41 +0900

    feat : ios 로그인 기능 추가

--- lib/src/app/page/login.dart
+++ lib/src/app/page/login.dart
@@ -7,7 +7,11 @@ import 'package:google_search_api/src/app/page/home.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 
+
+
 class Login extends StatelessWidget {
+
+
   Future<UserCredential> signInWithGoogle() async {
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
     final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
@@ -57,9 +61,11 @@ class Login extends StatelessWidget {
                 }
                 // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
                 try {
+
                 await UserApi.instance.loginWithKakaoAccount();
                 print('카카오계정으로 로그인 성공');
                 } catch (error) {
+
                 print('카카오계정으로 로그인 실패 $error');
                 }
                 }
