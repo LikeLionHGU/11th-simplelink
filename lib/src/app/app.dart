a951619cd212ea088fd56c0e091e3d7495c244c4
chamroro <chamsae@handong.ac.kr>
2023-08-16 17:46:03 +0900

    feat : chatgpt api + google search api

--- lib/src/app/app.dart
+++ lib/src/app/app.dart
@@ -3,23 +3,24 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_search_api/src/app/page/googleSearch/googleSearch.dart';
 import 'package:google_search_api/src/app/page/home.dart';
+import 'page/home_page.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: Firebase.initializeApp(),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Center(
-            child: Text("firebase load fail"),
-          );
-        }
-        if (snapshot.connectionState == ConnectionState.done) {
-          return SearchForm();
-        }
-        return CircularProgressIndicator();
-      },
-    );
+    // return FutureBuilder(
+    //   future: Firebase.initializeApp(),
+    //   builder: (context, snapshot) {
+    //     if (snapshot.hasError) {
+    //       return const Center(
+    //         child: Text("firebase load fail"),
+    //       );
+    //     }
+    //     if (snapshot.connectionState == ConnectionState.done) {
+    return const HomePage();
+    // }
+    //     return const CircularProgressIndicator();
+    //   },
+    // );
   }
 }
